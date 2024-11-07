1484bcec340a58a84f637b0f2e87dcdc8183527c
vst212 <47769540+vst212@users.noreply.github.com>
2024-11-07 21:24:18 +0800

    Update baccaratMartingale_v8.1.js

--- client/baccaratMartingale_v8.1.js
+++ client/baccaratMartingale_v8.1.js
@@ -87,7 +87,7 @@ let balance = config.funds.available,
     tied = false,
     lost = false,
     // Baccarat Bet Settings
-    baseBet = 0.000062,
+    baseBet = 0.000063,
     previousBet = baseBet,
     nextBet = baseBet,
     tieBet = 0, // Baccarat tie bet amount
