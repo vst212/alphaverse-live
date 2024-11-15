554a824f68f1b196e7af4e8887a2f61ad5167dd4
vst212 <47769540+vst212@users.noreply.github.com>
2024-11-16 01:22:22 +0800

    Update buildAgent.sh

--- bin/buildAgent.sh
+++ bin/buildAgent.sh
@@ -1,20 +1,21 @@
-#!/bin/bash -l
+#!/bin/bash -l  # Login shell for proper nvm initialization
 
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="$HOME/.nvm"  # Use $HOME here, it's more reliable
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 nvm use v23.1.0
 
+# Debugging lines (keep these for now)
 command -v nvm
 nvm --version
 which yarn
 echo $PATH
 
-YARN_PATH="/home/tintin/.nvm/versions/node/v23.1.0/bin/yarn"  # Full path, no tilde
+YARN_PATH="/home/tintin/.nvm/versions/node/v23.1.0/bin/yarn"  # Absolute path, NO TILDE
 
 echo 'Starting execution...'
-cd
+cd /home/tintin # Ensure you're in the correct home directory
 rm -rf proxy
 mkdir proxy
-cp -r ~/alphaverse-live/agent/* ~/proxy/ # Tilde is OK here because it's a direct shell command
+cp -r ~/alphaverse-live/agent/* ~/proxy/ # Tilde is OK *here* because it's a direct shell command
 cd proxy/
 $YARN_PATH install
