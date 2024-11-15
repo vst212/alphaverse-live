8207f33200a5e30963a2dc24c9e73adbe923b0da
vst212 <47769540+vst212@users.noreply.github.com>
2024-11-16 01:34:16 +0800

    Update buildAgent.sh

--- bin/buildAgent.sh
+++ bin/buildAgent.sh
@@ -1,13 +1,10 @@
 #!/bin/bash -l
 
-# OR, if -l is still problematic:
-# #!/bin/bash
-# if [ -f /etc/profile ]; then . /etc/profile; fi
-# if [ -f ~/.bash_profile ]; then . ~/.bash_profile; elif [ -f ~/.bash_login ]; then . ~/.bash_login; elif [ -f ~/.profile ]; then . ~/.profile; fi
-
-
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Ensure Node.js v23.1.0 is installed and used
+nvm install v23.1.0  # Or nvm use v23.1.0 if it's already installed
 nvm use v23.1.0
 
 # Debugging lines (keep these for now)
@@ -16,12 +13,11 @@ nvm --version
 which yarn
 echo $PATH
 
-YARN_PATH="/home/tintin/.nvm/versions/node/v23.1.0/bin/yarn"  # Absolute path, NO TILDE
-
+# No need to set YARN_PATH manually anymore. Use nvm exec:
 echo 'Starting execution...'
 cd /home/tintin
 rm -rf proxy
 mkdir proxy
 cp -r ~/alphaverse-live/agent/* ~/proxy/
 cd proxy/
-$YARN_PATH install
+nvm exec v23.1.0 yarn install # Or nvm exec <version> yarn install if using a different version
