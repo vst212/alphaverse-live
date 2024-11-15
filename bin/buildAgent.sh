132c5c9b65514ff2e67df4a3c5312f515f560085
vst212 <47769540+vst212@users.noreply.github.com>
2024-11-16 01:18:00 +0800

    Update buildAgent.sh

--- bin/buildAgent.sh
+++ bin/buildAgent.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-YARN_PATH="~/.nvm/versions/node/v23.1.0/bin/yarn"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm use v23.1.0  # Or your specific Node.js version
+
+YARN_PATH="/home/tintin/.nvm/versions/node/v23.1.0/bin/yarn"
 
 echo 'Starting execution...'
 cd
@@ -8,4 +12,4 @@ rm -rf proxy
 mkdir proxy
 cp -r ~/alphaverse-live/agent/* ~/proxy/
 cd proxy/
-$YARN_PATH install  # Use the defined YARN_PATH here
+$YARN_PATH install
